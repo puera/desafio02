15f2d5a6f911f9310c27af35692289d3403aac21
Renann Souza <renann_puera@hotmail.com>
2020-01-30 15:23:19 -0300

    Create JWT token and auth  / add SessionController

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found!' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password invalid!' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, process.env.JWT_KEY, {
+        expiresIn: process.env.JWT_EXPIRES,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
