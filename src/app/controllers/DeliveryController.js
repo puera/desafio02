e5d64c26e64b8eb6f08ccc8c78ba569b64ac115a
Renann Souza <renann_puera@hotmail.com>
2020-03-06 16:37:45 -0300

    Include Signature  on request DeliveryController

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -100,6 +100,11 @@ class DeliveryController {
             },
           ],
         },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['name', 'path', 'url'],
+        },
       ],
     });
 
