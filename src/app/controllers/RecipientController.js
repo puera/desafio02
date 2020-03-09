fefb93a6b82209a7805df82eb9cbd04f3524b0a5
Renann Souza <renann_puera@hotmail.com>
2020-03-09 20:01:47 -0300

    Remove cep-promise and refactor store method on RecipientController

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,6 +1,5 @@
 import * as Yup from 'yup';
 import { Op } from 'sequelize';
-import zipCode from 'cep-promise';
 
 import Recipient from '../models/Recipient';
 
@@ -68,30 +67,20 @@ class RecipientController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
+      street: Yup.string().required(),
       number: Yup.string().required(),
-      complement: Yup.string().required(),
-      zip_code: Yup.string().required(),
+      complement: Yup.string(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip: Yup.string().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails!' });
     }
-    try {
-      const data = await zipCode(req.body.zip_code);
-      const { zip = data.cep } = data;
-      const { name, number, complement } = req.body;
+    const recipient = await Recipient.create(req.body);
 
-      const recipient = await Recipient.create({
-        name,
-        number,
-        complement,
-        zip,
-        ...data,
-      });
-      return res.json({ recipient });
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
+    return res.json(recipient);
   }
 
   async update(req, res) {
